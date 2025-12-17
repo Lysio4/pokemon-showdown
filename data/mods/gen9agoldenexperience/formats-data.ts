8e70ffb343fc6004726fdd87d596364c65aa8c22
Lysio4 <lukas.moncorge@proton.me>
2025-12-17 14:41:14 +0100

    quick add

--- data/mods/gen9agoldenexperience/formats-data.ts
+++ data/mods/gen9agoldenexperience/formats-data.ts
@@ -3541,7 +3541,7 @@ export const FormatsData: { [k: string]: SpeciesFormatsData; } = {
 		doublesTier: "DUU",
 	},
 	hawluchamega: {
-		tier: "UUBL",
+		tier: "OU",
 		doublesTier: "DUU",
 	},
 	dedenne: {

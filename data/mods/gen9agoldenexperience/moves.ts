4705838b07c921c30bf8db10be716cdca77dc582
Lysio4 <lukas.moncorge@proton.me>
2025-12-18 14:49:52 +0100

    good

--- data/mods/gen9agoldenexperience/moves.ts
+++ data/mods/gen9agoldenexperience/moves.ts
@@ -3910,7 +3910,7 @@ export const Moves: { [k: string]: ModdedMoveData; } = {
 		basePower: 100,
 		category: "Physical",
 		name: "Season Pass",
-		pp: 5,
+		pp: 10,
 		priority: 0,
 		flags: { contact: 1, protect: 1, mirror: 1, metronome: 1 },
 		onModifyType(move, pokemon) {
--- data/moves.ts
+++ data/moves.ts
@@ -24948,7 +24948,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		basePower: 100,
 		category: "Physical",
 		name: "Season Pass",
-		pp: 5,
+		pp: 10,
 		priority: 0,
 		flags: { contact: 1, protect: 1, mirror: 1, metronome: 1 },
 		onModifyType(move, pokemon) {

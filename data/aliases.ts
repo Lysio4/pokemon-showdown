a85ac91a3d6899af82a4783695db1287bd4dbe3c
Lysio4 <lukas.moncorge@proton.me>
2025-12-18 14:05:34 +0100

    without the alias now

--- data/aliases.ts
+++ data/aliases.ts
@@ -142,8 +142,6 @@ export const Aliases: import('../sim/dex').AliasesTable = {
 	megasaur: "Venusaur-Mega",
 	megazygardecomplete: "Zygarde-Mega",
 	zygardecompletemega: "Zygarde-Mega",
-	// custom
-	sawsbuckmega: "Sawsbuck-Spring-Mega",
 
 	// Pokéstar Studios --- 1st ordered alphabetically by species, 2nd by alias
 	blackdoor: "Pokestar Black Door",

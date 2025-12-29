6adfec2ad4befe793a7d1ce72e9ea74446b3c781
Lysio4 <lukas.moncorge@proton.me>
2025-12-29 17:41:20 +0100

    back in business

--- data/mods/gen9agoldenexperience/rulesets.ts
+++ data/mods/gen9agoldenexperience/rulesets.ts
@@ -19,7 +19,8 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 		onValidateSet(set) {
 			const item = this.dex.items.get(set.item);
 			const sigZcrystals = ["Pikanium Z", "Pikashunium Z", "Aloraichium Z", "Eevium Z", "Snorlium Z", "Mewnium Z", "Decidium Z", "Incinium Z", "Primarium Z", "Lycanium Z", "Mimikium Z", "Kommonium Z", "Tapunium Z", "Solganium Z", "Lunalium Z", "Ultranecrozium Z", "Marshadium Z"];
-			if (item.zMove && !sigZcrystals.includes(item.name)) return [`${set.name || set.species}'s item ${item.name} is banned by Non-Signature Z-Move Clause.`];
+			// if (item.zMove && !sigZcrystals.includes(item.name)) return [`${set.name || set.species}'s item ${item.name} is banned by Non-Signature Z-Move Clause.`];
+			if (item.zMove && !item.itemUser) return [`${set.name || set.species}'s item ${item.name} is banned by Non-Signature Z-Move Clause.`];
 		},
 		onBegin() {
 			this.add('rule', 'Non-Signature Z-Move Clause: non-signature Z-Moves are banned');

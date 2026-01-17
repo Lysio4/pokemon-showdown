a2edc8a7b8f07658a6d3a365534386af27e46a7c
Lysio4 <lukas.moncorge@proton.me>
2026-01-17 15:06:24 +0100

    clean

--- config/formats.ts
+++ config/formats.ts
@@ -182,7 +182,6 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 			'Uber', 'AG', 'OU', 'UUBL', 'Moody', 'Power Construct', 'King\'s Rock',
 			'Baton Pass', 'Last Respects', 'Quick Claw', 'Rage Fist', 'Razor Fang', 'Shed Tail',
 			'Drizzle', 'Drought', 'Light Clay', 
-			/*'Zoroark-Hisui + Zoroarkite', */
 		],
 		unbanlist: ['Light of Ruin'],
 	},
@@ -195,7 +194,6 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 			'Uber', 'AG', 'OU', 'UUBL', 'UU', 'RUBL', 'Moody', 'Power Construct', 'King\'s Rock',
 			'Baton Pass', 'Last Respects', 'Quick Claw', 'Rage Fist', 'Razor Fang', 'Shed Tail',
 			'Drizzle', 'Drought', 'Light Clay', 
-			/*'Typhlosion + Typhlosionite', */
 		],
 		unbanlist: ['Light of Ruin'],
 	},

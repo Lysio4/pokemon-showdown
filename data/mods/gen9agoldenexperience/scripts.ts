fa5fb11492bed2ca8cf55dc06fa50aa77d9e08d6
Lysio4 <lukas.moncorge@proton.me>
2026-04-13 17:00:18 +0200

    this time it WILL work

--- data/mods/gen9agoldenexperience/scripts.ts
+++ data/mods/gen9agoldenexperience/scripts.ts
@@ -9,7 +9,26 @@ export const Scripts: ModdedBattleScriptsData = {
     if (this.gen <= 2 && move.pp === 40) pp -= ppUps;
     return pp;
   },
-  statModify(baseStats, set, statName) {},
+
+  statModify(baseStats: StatsTable, set: PokemonSet, statName: StatID): number {
+    const tr = this.trunc;
+    let stat = baseStats[statName];
+    if (statName === 'hp') {
+      return tr(tr(2 * stat + set.ivs[statName] + tr(set.evs[statName] / 4) + 100) * set.level / 100 + 10);
+    }
+    stat = tr(tr(2 * stat + set.ivs[statName] + tr(set.evs[statName] / 4)) * set.level / 100 + 5);
+    const nature = this.dex.natures.get(set.nature);
+    // Natures are calculated with 16-bit truncation.
+    // This only affects Eternatus-Eternamax in Pure Hackmons.
+    if (nature.plus === statName) {
+      stat = this.ruleTable.has('overflowstatmod') ? Math.min(stat, 595) : stat;
+      stat = tr(tr(stat * 110, 16) / 100);
+    } else if (nature.minus === statName) {
+      stat = this.ruleTable.has('overflowstatmod') ? Math.min(stat, 728) : stat;
+      stat = tr(tr(stat * 90, 16) / 100);
+    }
+    return stat;
+  },
   actions: {
     runSwitch(pokemon: Pokemon) {
       const switchersIn = [pokemon];

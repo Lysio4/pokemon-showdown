ed9b75df05b39b0a5344e9d84f1038f3d5cf3c62
Lysio4 <lukas.moncorge@proton.me>
2026-04-29 16:56:08 +0200

    ND COU

--- config/formats.ts
+++ config/formats.ts
@@ -34,6 +34,15 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		ruleset: ['PotD', 'Obtainable', 'Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Illusion Level Mod'],
 	},
 	{
+		name: "[Gen 9 Champions] National Dex OU",
+		mod: 'champions',
+		ruleset: ['Standard NatDex'],
+		banlist: [
+			'ND Uber', 'ND AG', 'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag', 'King\'s Rock',
+			'Quick Claw', 'Razor Fang', 'Assist', 'Baton Pass', 'Last Respects', 'Shed Tail',
+		],
+	},
+	{
 		name: "[Gen 9 Champions] OU",
 		mod: 'champions',
 		ruleset: ['Standard'],

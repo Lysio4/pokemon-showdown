d63d7b6ec13ef59256575ed68b5a8f52f30b7947
Lysio4 <lukas.moncorge@proton.me>
2026-08-22 16:52:48 +0200

    trying to fix mimikyu mega

--- data/mods/gen9agoldenexperience/scripts.ts
+++ data/mods/gen9agoldenexperience/scripts.ts
@@ -53,25 +53,6 @@ export const Scripts: ModdedBattleScriptsData = {
       return item.megaStone?.[species.name] || null;
     },
 
-	runMegaEvo(pokemon: Pokemon) {
-		const speciesid = pokemon.canMegaEvo || pokemon.canUltraBurst;
-		if (!speciesid) return false;
-
-		// pokemon.formeChange(speciesid, pokemon.getItem(), true);
-
-		// Limit one mega evolution
-		const wasMega = pokemon.canMegaEvo;
-		for (const ally of pokemon.side.pokemon) {
-			if (wasMega) {
-				ally.canMegaEvo = false;
-			} else {
-				ally.canUltraBurst = null;
-			}
-		}
-
-		this.battle.runEvent('AfterMega', pokemon);
-		return true;
-	},
     runSwitch(pokemon: Pokemon) {
       const switchersIn = [pokemon];
       while (this.battle.queue.peek()?.choice === 'runSwitch') {
